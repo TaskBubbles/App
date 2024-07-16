365659e394b77891f3051a46b11328a272381e40
GuyX10 <guyguyeks@gmail.com>
2024-07-16 20:02:13 +0300

    change default task title

--- variables.js
+++ variables.js
@@ -2,7 +2,7 @@
 const ColorScheme = ['#db522c', '#edb71f', '#15ba57', '#22c5f9', '#832cff'];
 
 let defaultBubbleSize = 200 - Math.sqrt(window.innerWidth * 15);
-let defaultTaskTitle = "Task Name";
+let defaultTaskTitle = "Task";
 let editCancelDelay = 200;
 let popHoldDelay = 1000;
 let editPosition = { x: window.innerWidth / 2, y: window.innerHeight / 4 };
